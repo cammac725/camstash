4c4026ec6c2ed5ed762a1f2e7ecf3eeebf63f973
cammac <camm@rocktechcomputer.com>
2022-03-07 12:12:29 -0500

    add localStorage, Book.js

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddBook from '../components/AddBook';
 import Bookslist from '../components/Bookslist';
 import Header from '../components/Header';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const AppRouter = () => {
+  const [books, setBooks] = useLocalStorage('books', []);
+
   return (
     <BrowserRouter>
       <div>
         <Header />
         <div className='main-content'>
-          <Routes>
-            <Route element={<Bookslist/>} path="/" exact />
-            <Route element={<AddBook/>} path="/add" />
-          </Routes>
+          <Switch>
+            <Route 
+              render={(props) => (
+                <Bookslist {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/" exact />
+            <Route 
+              render={(props) => (
+                <AddBook {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/add" />
+          </Switch>
         </div>
       </div>
     </BrowserRouter>
